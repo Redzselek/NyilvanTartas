1635c7afbc2f748e42596de73bad024c6c64531e
Egyedi Róbert <egyedirobika@gmail.com>
2023-09-06 14:00:25 +0200

    2.óra

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,27 @@ function Hozzaad()
 {
     var obj = { 
     nev : document.getElementById("nev").value,   
-    eletkor : parseINT(document.getElementById("eletkor").value),   
-    magassag : parseINT(document.getElementById("magassag").value),   
-    hazas : document.getElementById("hazas").checked,    
+    eletkor : parseInt(document.getElementById("eletkor").value),   
+    magassag : parseInt(document.getElementById("magassag").value),   
+    hazas : document.getElementById("hazas").checked 
     }
     emberek.push(obj)
-    
+    TablaKirajzol() 
 }
+
+function TablaKirajzol() {
+    var s = "<table><tr><th>Név</th><th>Kor</th><th>Magasság</th><th>Házas</th></tr>"
+    for (let i = 0; i < emberek.length; i++) {
+        s += "<tr><td>"+emberek[i].nev+"</td><td>"+emberek[i].kor+"</td><td>"+emberek[i].magassag+"</td><td>"+emberek[i].hazas+"</td><tr>"
+        if (emberek[i].hazas) {
+            s += "Igen</td></tr>"
+        }
+        else
+        {
+            s+="Nem</td></tr>"
+        }
+    }
+    s+="</table>"
+    document.getElementById("tablazat").innerHTML = s;
+}
+TablaKirajzol()
